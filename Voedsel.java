0c93fbdda79d4233de57ac504f7ecf0eaab63a25
LJHoen <ljp.hoen@gmail.com>
2019-01-23 13:13:36 +0100

    start

---
+++ Voedsel.java
@@ -0,0 +1,38 @@
+package kantine;
+
+public class Voedsel {
+	
+}
+
+class Uiensoep extends Voedsel {
+	
+}
+
+class Groentensoep extends Voedsel implements Vegetarisch {
+	
+}
+
+class ThaiseRundersalade extends Voedsel {
+	
+}
+
+class Tonijnsalade extends Voedsel {
+	
+}
+
+class Geitenkaassalade extends Voedsel implements Vegetarisch {
+
+}
+
+class BroodjeKaas extends Voedsel implements Vegetarisch {
+	
+}
+
+class BroodjeHam extends Voedsel{
+	
+}
+
+interface Vegetarisch {
+	
+	
+}
