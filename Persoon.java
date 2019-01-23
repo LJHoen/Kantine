0309114ff89b4ab6a8bf946d5f97cb4261888786
LJHoen <ljp.hoen@gmail.com>
2019-01-23 13:44:06 +0100

    start

--- Persoon.java
+++ Persoon.java
@@ -1,9 +1,34 @@
 package kantine;
+import java.util.*;
 
 public class Persoon {
-
+	boolean vegetarier;
+	double geld;
+	int honger;
+	
+	Persoon(double geld, int honger, boolean vegetarier) {
+		this.geld = geld;
+		this.honger = honger;
+		this.vegetarier = vegetarier;
+	}
+	
+	Voedsel kopen(ArrayList<Voedsel> menu) {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		Voedsel v = menu.get(input+1);
+		
+		
+		return v;
+	}
+	
+	
+	void eten() {
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 	
